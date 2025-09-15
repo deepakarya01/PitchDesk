fc257b3471f08a4de0baee46486def816cbb917c
Deepak <harsh1206arya@gmail.com>
2025-09-15 10:13:33 +0530

    Fix Auth.ts Github Provider

--- app/auth.ts
+++ app/auth.ts
@@ -1,3 +1,4 @@
+// app/auth.ts
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
@@ -5,7 +6,12 @@ import prisma from './lib/db';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub],
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
   callbacks: {
     async session({ session, user }) {
       if (user) {
